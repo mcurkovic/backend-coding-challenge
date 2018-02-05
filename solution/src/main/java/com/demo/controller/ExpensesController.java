13b75b05de51bfe1fccc225b3f580245ff6230b4
mcurkovic <matija.curkovic@gmail.com>
2018-02-05 12:15:57 +0100

    Add orika mapper: mapping DTO to Entity and vice versa

--- solution/src/main/java/com/demo/controller/ExpensesController.java
+++ solution/src/main/java/com/demo/controller/ExpensesController.java
@@ -6,14 +6,10 @@ import com.demo.domain.ConversionResult;
 import com.demo.domain.Expense;
 import com.demo.domain.Money;
 import com.demo.services.api.ConversionManager;
-import com.demo.services.api.ExchangeRatesManager;
 import com.demo.services.api.ExpensesManager;
 import com.demo.services.api.TaxManager;
-import java.math.BigDecimal;
-import java.util.List;
 import javax.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import ma.glasnost.orika.MapperFacade;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -22,63 +18,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/app")
 public class ExpensesController {
-    //see application.properties taxRatePercent
-    private final BigDecimal taxRatePercent;
 
     private final ExpensesManager expensesManager;
-
-    private final ExchangeRatesManager exchangeRatesManager;
 
     private final TaxManager taxManager;
 
     private final ConversionManager conversionManager;
 
-    @Autowired
-    public ExpensesController(@Value("${taxRatePercent}") String taxRatePercent, ExpensesManager expensesManager,
-            ExchangeRatesManager exchangeRatesManager, TaxManager taxManager, ConversionManager conversionManager) {
+    //orika mapper used for mapping DTO to Entity and vice versa.
+    //For mappers injection see @com.demo.utils.SpringConfigurableMapper
+    private final MapperFacade mapperFacade;
 
-        try {
-            final BigDecimal taxRate = new BigDecimal(taxRatePercent);
-            this.taxRatePercent = taxRate;
-        } catch (Exception e) {
-            throw new IllegalArgumentException("taxRatePercent misconfigured. Use format 00.00 e.g. 20.00");
-        }
-
+    public ExpensesController(ExpensesManager expensesManager, TaxManager taxManager,
+            ConversionManager conversionManager, MapperFacade mapperFacade) {
         this.expensesManager = expensesManager;
-        this.exchangeRatesManager = exchangeRatesManager;
         this.taxManager = taxManager;
         this.conversionManager = conversionManager;
+        this.mapperFacade = mapperFacade;
     }
 
     @RequestMapping(value = "/expenses", method = RequestMethod.POST)
     public ExpenseDTO saveExpense(@Valid @RequestBody ExpenseDTO command) {
-
-        //calculate domestic amount and tax
-        final Money amount = new Money(command.getAmount(), command.getCurrencyCode());
-        final ConversionResult conversionResult = conversionManager.convertToDomesticAmount(amount, command.getDate());
-        final Money taxAmount = taxManager.calculateTaxAmount(conversionResult.getDomesticAmount());
-        final Expense expense = convertToExpense(command, amount, conversionResult, taxAmount);
+        final Expense expense = mapperFacade.map(command, Expense.class);
         final Expense savedExpense = expensesManager.saveExpense(expense);
-
-        //return populated command
+        final ExpenseDTO populatedDTO = mapperFacade.map(savedExpense, ExpenseDTO.class);
         return command;
-    }
-
-    private Expense convertToExpense(ExpenseDTO command, Money amount,
-            ConversionResult conversionResult, Money taxAmount)
-    {
-        //map dto to entity - usually use library such as Orika Mapper, for demo in controller.
-        final Expense expense = new Expense();
-        expense.setDomesticAmount(conversionResult.getDomesticAmount());
-        expense.setAmount(amount);
-        expense.setTaxAmount(taxAmount);
-        expense.setExpenseDate(command.getDate());
-        expense.setReason(command.getReason());
-        expense.setTaxRate(taxRatePercent);
-        expense.setExchangeRate(conversionResult.getRate());
-        expense.setExchangeRateDate(command.getDate());
-        expense.setUserId(Long.valueOf(1));
-        return expense;
     }
 
 
---
+++ solution/src/main/java/com/demo/utils/SpringConfigurableMapper.java
@@ -0,0 +1,102 @@
+package com.demo.utils;
+
+import java.util.Map;
+import ma.glasnost.orika.Converter;
+import ma.glasnost.orika.Mapper;
+import ma.glasnost.orika.MapperFacade;
+import ma.glasnost.orika.MapperFactory;
+import ma.glasnost.orika.impl.ConfigurableMapper;
+import ma.glasnost.orika.impl.DefaultMapperFactory;
+import ma.glasnost.orika.metadata.ClassMapBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * A bean mapper designed for Spring suitable for dependency injection. Provides
+ * an implementation of {@link MapperFacade} which can be injected. In addition
+ * it is "Spring aware" in that it can autodiscover any implementations of
+ * {@link Mapper} or {@link Converter} that are managed beans within it's parent
+ * {@link ApplicationContext}.
+ */
+@Component
+public class SpringConfigurableMapper extends ConfigurableMapper implements
+ApplicationContextAware {
+
+	private MapperFactory factory;
+	private ApplicationContext applicationContext;
+
+	public SpringConfigurableMapper() {
+		super(false); // delay the initialization
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected void configureFactoryBuilder(
+			final DefaultMapperFactory.Builder factoryBuilder) {
+		// customize the factoryBuilder as needed
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected void configure(final MapperFactory p_factory) {
+		this.factory = p_factory;
+		// customize the factory as needed
+		addAllSpringBeans(applicationContext);
+	}
+
+	@Override
+	@Autowired
+	public void setApplicationContext(
+			final ApplicationContext p_applicationContext) {
+		applicationContext = p_applicationContext;
+		init();
+	}
+
+	/**
+	 * Adds all managed beans of type {@link Mapper} or {@link Converter} to the
+	 * parent {@link MapperFactory}.
+	 *
+	 * @param applicationContext
+	 *            The application context to look for managed beans in.
+	 */
+	@SuppressWarnings({ "rawtypes" })
+	private void addAllSpringBeans(final ApplicationContext p_applicationContext) {
+		final Map<String, Converter> converters = p_applicationContext
+				.getBeansOfType(Converter.class);
+
+		for (Map.Entry<String, Converter> converter : converters.entrySet())
+			addConverter(converter.getKey(), converter.getValue());
+
+		final Map<String, Mapper> mappers = p_applicationContext
+				.getBeansOfType(Mapper.class);
+		for (final Mapper mapper : mappers.values()) {
+			addMapper(mapper);
+		}
+
+	}
+
+	private void addConverter(String converterId,
+			final Converter<?, ?> converter) {
+		factory.getConverterFactory().registerConverter(converterId, converter);
+	}
+
+	/**
+	 * Add a {@link Mapper}.
+	 *
+	 * @param mapper
+	 *            The mapper.
+	 */
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public void addMapper(final Mapper<?, ?> mapper) {
+		ClassMapBuilder<?, ?> classMapBuilder = factory.classMap(
+				mapper.getAType(), mapper.getBType());
+
+		classMapBuilder.customize((Mapper) mapper).register();
+	}
+}
